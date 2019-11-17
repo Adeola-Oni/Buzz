d0f00182fd1680ccd71dd73c299b6495cabcaa24
Adeola-Oni <oniadeola33@gmail.com>
2019-11-18 00:37:53 +0100

    second commit

--- src/Page/Side1.js
+++ src/Page/Side1.js
@@ -14,48 +14,46 @@ function Content() {
     <div>
         <br/>
         <section>
-            <Card style={{padding: '3%', color: 'black'}} className='side1-card'>
-                <div style={{Align: 'center'}}>
-                    <h6 style={{textAlign: 'left'}}>Text Describing Images/Story</h6>
-                    <Container>
-                        <Row>
-                            <Col sm={4} className='pic1 box'></Col>
-                            <Col sm={4} className='pic2 box'></Col>
-                            <Col sm={4} className='pic3 box'></Col>
-                        </Row>
-                        <br/>
-                        <ul>
-                            <li><img src={BBC} style={{width:'10%', borderRadius: '50%'}}/> New York post</li>
-                            <li><img src={comment}/> 236</li>
-                            <li><img src={time}/> 2:56pm</li>
-                        </ul>  
-                    </Container>
-                </div>
-            </Card>
+            <div style={{Align: 'center'}}>
+                <h6 style={{textAlign: 'left'}}>Text Describing Images/Story</h6>
+                <Container>
+                    <Row>
+                        <Col sm={4} className='pic1 box'></Col>
+                        <Col sm={4} className='pic2 box'></Col>
+                        <Col sm={4} className='pic3 box'></Col>
+                    </Row>
+                    <br/>
+                    <ul>
+                        <li><img src={BBC} style={{width:'10%', borderRadius: '50%'}}/> New York post</li>
+                        <li><img src={comment}/> 236</li>
+                        <li><img src={time}/> 2:56pm</li>
+                    </ul>  
+                </Container>
+            </div>
+            <br/><br/>
         </section>
         <br/>
         <section>
-            <Card style={{padding: '3%', }} className='side1-card'>
-                <div style={{Align: 'center'}}>
-                    <Container>
-                        <Row>
-                            <Col sm={6} style={{textAlign:'left'}}><h6>Text Describing Story</h6>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                                 voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-                                 non proident.</p></Col>
-                            <Col sm={6} className='pic4 box'></Col>
-                        </Row>
-                        <br/>
-                        <ul>
-                            <li><img src={BBC} style={{width:'10%', borderRadius: '50%'}}/> New York post</li>
-                            <li><img src={comment}/> 236</li>
-                            <li><img src={time}/> 2:56pm</li>
-                        </ul>
-                    </Container>
-                </div>
-            </Card>
+            
+            <div style={{Align: 'center'}}>
+                <Container>
+                    <Row>
+                        <Col sm={6} style={{textAlign:'left'}}><h6>Text Describing Story</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                                non proident.</p></Col>
+                        <Col sm={6} className='pic4 box'></Col>
+                    </Row>
+                    <br/>
+                    <ul>
+                        <li><img src={BBC} style={{width:'10%', borderRadius: '50%'}}/> New York post</li>
+                        <li><img src={comment}/> 236</li>
+                        <li><img src={time}/> 2:56pm</li>
+                    </ul>
+                </Container>
+            </div>
         </section>
     </div>
   );
